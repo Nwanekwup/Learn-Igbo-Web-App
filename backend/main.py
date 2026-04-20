93cb16d32dcab979067d0d965fdb79eaa9ed08bb
Princess Nwanekwu <127428375+Nwanekwup@users.noreply.github.com>
2026-04-20 13:48:35 -0500

    chore: add requirements.txt and update CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -19,9 +19,9 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["*"], 
+    allow_methods=["*"],  
     allow_headers=["*"],  
 )
 
